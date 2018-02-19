e7b0eba43f7009306402b46516b981ef45f77c4e
Julien Deniau <julien.deniau@gmail.com>
2018-02-19 17:38:14 +0100

    Support for PHP 7.1 & PHP 7.2

---
+++ CHANGELOG.md
@@ -0,0 +1,12 @@
+CHANGELOG
+===========
+
+## 2.0.0
+### Added 
+Support for PHP 7.1 and 7.2
+
+## Changed
+[Breaking] The classname changed from `Mapado\CS\Config\Php70` to `Mapado\Cs\Config`
+
+## 1.0.0
+Initial version, compatible with php 7
---
+++ src/Config.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Mapado\CS;
+
+use PhpCsFixer\Config as CsFixerConfig;
+
+final class Config extends Config
+{
+    private $useRisky;
+
+    public function __construct($useRisky = true)
+    {
+        parent::__construct('Mapado');
+
+        $this->setRiskyAllowed(true);
+        $this->useRisky = $useRisky;
+    }
+
+    public function getRules()
+    {
+        $out = [
+            '@PSR2' => true,
+            '@Symfony' => true,
+            '@PHP70Migration' => true,
+            '@PHP70Migration:risky' => $this->useRisky,
+            'array_syntax' => [ 'syntax' => 'short' ],
+            'concat_space' => [ 'spacing' => 'one' ],
+            'phpdoc_align' => false,
+            'phpdoc_summary' => false,
+            'ordered_imports' => true,
+            'ordered_class_elements' => true,
+        ];
+
+        if (version_compare(PHP_VERSION, '7.1.0', '>=')) {
+            $out += [
+                '@PHP71Migration' => true,
+                '@PHP71Migration:risky' => $this->useRisky,
+            ];
+        }
+
+        return $out;
+    }
+}
