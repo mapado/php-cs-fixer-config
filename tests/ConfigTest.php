002835d484b0c268b43e32eaf5196d577a60a1f1
Julien Deniau <julien.deniau@gmail.com>
2023-09-18 06:27:26 +0000

    Migrate from atoum to phpunit

---
+++ tests/ConfigTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Mapado\CS\Tests\Units;
+
+use Mapado\CS\Config;
+use PHPUnit\Framework\TestCase;
+
+class ConfigTest extends TestCase
+{
+    public function testConfig()
+    {
+        $config = new Config();
+        $rules = $config->getRules();
+        $this->assertNotEmpty($rules);
+        $this->assertTrue($rules['@PSR12']);
+    }
+
+    public function testRiskyRules()
+    {
+        $config = new Config();
+        $rules = $config->getRules();
+        $this->assertTrue($rules['@PHP70Migration:risky']);
+
+        $config = new Config(false);
+        $rules = $config->getRules();
+        $this->assertFalse($rules['@PHP70Migration:risky']);
+    }
+
+    public function testExtraRules()
+    {
+        $config = new Config(true, [
+            'visibility_required' => true,
+        ]);
+        $rules = $config->getRules();
+        $this->assertTrue($rules['visibility_required']);
+    }
+
+    public function testExtraRulesPosition()
+    {
+        $config = new Config(true, [
+            '@Symfony' => false,
+            'visibility_required' => false,
+        ]);
+        $rules = $config->getRules();
+        $symfonyPosition = array_search('@Symfony', array_keys($rules));
+        $this->assertEquals(1, $symfonyPosition);
+        $visiPosition = array_search('visibility_required', array_keys($rules));
+        $this->assertEquals(21, $visiPosition);
+    }
+}
